fb75b4667b7c53d48f506ee3cacc7632202dcbeb
Omar-Abdulmutaleb <omar197mohamed@gmail.com>
2025-05-02 22:26:59 +0300

    trial commit

---
+++ src/main/java/com/example/tryingOpenShift/TryingOpenShiftApplication.java
@@ -0,0 +1,14 @@
+package com.example.tryingOpenShift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryingOpenShiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryingOpenShiftApplication.class, args);
+		System.out.println("Petit Hughiee");
+	}
+
+}
